3a40bbec6ce6cb588ada8003969ee7acaadd7b17
ponakilan <ponakilan25@gmail.com>
2025-03-08 09:20:53 +0530

    Embedding generation

--- core/embeddings.py
+++ core/embeddings.py
@@ -1,4 +1,15 @@
 from openai import OpenAI
 
 
-client = OpenAI()
+class Embeddings:
+    def __init__(self, client: OpenAI=None):
+        self.client = client
+        if not client:
+            self.client = client = OpenAI()
+
+    def get_embeddings(self, text: str) -> list[float]:
+        response = self.client.embeddings.create(
+            input=text,
+            model="text-embedding-3-small"
+        )
+        return response.data[0].embedding
